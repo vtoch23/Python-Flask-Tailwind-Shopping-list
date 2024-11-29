98fb37333599e0e894174a1bf9a2e00db082dcdb
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-11-29 13:55:19 +0000

    simplified code and added error handling

--- main.py
+++ main.py
@@ -6,30 +6,45 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def get_all():
+    errorName = request.args.get('errorName', None)
+    errorPrice = request.args.get('errorPrice', None)
+
     records = get_items()
     total = total_cost()
     total_disc = total_discount()
-    total_p = total_paid()
-    return render_template('index.html', records=records, total=total, total_disc=total_disc, total_p=total_p)
+    paid = total_paid()
+
+    return render_template('index.html', records=records, total=total, total_disc=total_disc, total_paid=paid, errorName=errorName, errorPrice=errorPrice)
 
 
 @app.route('/add_item', methods=["POST"])
 def add():
-    name = request.form['name']
-    price = request.form['price']
-    discount = request.form['discount']
+    errorPrice = None
+    errorName = None
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        discount = request.form['discount']
 
-    add_item(name, price, discount)
+        if not name:
+            errorName = "Enter a valid name"
+        if not price:
+            errorPrice = "Enter a valid price"
 
-    return redirect(url_for('get_all'))
+        if errorName or errorPrice:
+            return redirect(url_for('get_all', errorName=errorName, errorPrice=errorPrice))
 
+        add_item(name, price, discount)
+        return redirect(url_for('get_all'))
 
 @app.route('/delete', methods=['POST'])
 def delete():
+    errorPrice = None
+    errorName = None
     id = request.form['id']
     delete_item(id)
 
-    return redirect(url_for('get_all'))
+    return redirect(url_for('get_all', errorName=errorName, errorPrice=errorPrice))
 
 
 if __name__ == '__main__':
